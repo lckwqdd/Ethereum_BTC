368456ad0be5ada2cac68f10abca3329c0605880
wuquan <17324432872@163.com>
2019-12-21 18:36:54 +0800

    1.添加冷钱包创建ui

---
+++ ALSC/alscMain/src/main/java/com/mirko/alsc/ui/wallet/WalletTypeActivity.java
@@ -0,0 +1,22 @@
+package com.mirko.alsc.ui.wallet;
+
+import android.os.Bundle;
+import com.alsc.find.base.AlscBaseActivity;
+import com.mirko.alsc.R;
+
+public class WalletTypeActivity extends AlscBaseActivity {
+    @Override
+    public void initViews(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_wallet_type);
+    }
+
+    @Override
+    public void initAttrs() {
+
+    }
+
+    @Override
+    public void loadData() {
+
+    }
+}

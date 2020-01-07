3a2a4bb22ace0cfd4d021aabc4d85db0d408a870
wuquan <17324432872@163.com>
2020-01-07 23:10:37 +0800

    1.提交增加代码

---
+++ ALSC/alscMain/src/main/java/com/mirko/alsc/ui/entity/ExpandData.java
@@ -0,0 +1,25 @@
+package com.mirko.alsc.ui.entity;
+
+import com.mirko.alsc.entity.Level0Item;
+import com.mirko.alsc.entity.Level1Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExpandData {
+
+
+    private List<Level0Item> level0Items = new ArrayList<>();
+
+
+
+    public List<Level0Item> getLevel0Items() {
+        return level0Items;
+    }
+
+    public void setLevel0Items(List<Level0Item> level0Items) {
+        this.level0Items = level0Items;
+    }
+
+
+}
